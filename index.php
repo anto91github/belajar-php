04c0aed5f0569b32fde25f5c0a91718ad5188097
anto91github <antoz91@yahoo.com>
2024-01-22 14:35:25 +0700

    test variable

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
+    //declare variable
+    $phi = 3.14;
 
+    //declare const
+    define('phi', 3.14);
+
+   function printVar()
+   {
+        global $phi;
+        echo $phi;
+   }
+
+   function printConst()
+   {
+        echo phi;
+   }
+
+   printVar();
+   echo '<br/>';
+   printConst();
 ?>
