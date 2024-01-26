0441d57ec0e445bbd64d082e69655b07c6a43e0d
anto91github <antoz91@yahoo.com>
2024-01-26 14:26:37 +0700

    get-post

--- index.php
+++ index.php
@@ -1,13 +1,31 @@
-<?php
- menyapa('budi',18);
- echo '<br>';
- menyapa('sarah',20);
- echo '<br>';
- menyapa('rendi');
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Document</title>
+</head>
+<style>
+     label {
+          display:block;
+     }
+</style>
+<body>
+     <h3>Input siswa baru</h3>
 
-function menyapa($nama, $umur=17){
-     echo 'selamat pagi '.$nama. 'umur kamu ' .$umur;
-}
-
-?>
+     <form action="proses.php" method="post">
+          <div>
+               <label for="nama">Nama</label>
+               <input type="text" name="nama" id="nama">
+          </div>
+          <div>
+               <label for="jurusan">Jurusan</label>
+               <input type="text" name="jurusan" id="jurusan">
+          </div>
+          <div>
+               <button type="submit" name="buttonKirim" value= "kirim"> Kirim</button>
+          </div>
+     </form>
+</body>
+</html>
 
