b8478c5580a0fe66a97e8079a792cc56b8991b11
anto91github <antoz91@yahoo.com>
2024-01-26 10:54:24 +0700

    looping

--- index.php
+++ index.php
@@ -1,22 +1,67 @@
 <?php
-    //declare variable
-    $phi = 3.14;
+    $siswa = [
+     ["nama" => "Budi", "kelas" => "XI", "jurusan" => "IPA"],
+     ["nama" => "Sinta", "kelas" => "XI", "jurusan" => "IPS"],
+     ["nama" => "Jojo", "kelas" => "XI", "jurusan" => "Bahasa"],
+     ["nama" => "Toni", "kelas" => "XI", "jurusan" => "Seni"],
+     ["nama" => "Santi", "kelas" => "XI", "jurusan" => "Drama"],
+];
 
-    //declare const
-    define('phi', 3.14);
-
-   function printVar()
-   {
-        global $phi;
-        echo $phi;
-   }
-
-   function printConst()
-   {
-        echo phi;
-   }
-
-   printVar();
-   echo '<br/>';
-   printConst();
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Document</title>
+</head>
+<style>
+     table{
+          border-collapse:collapse;
+     }
+</style>
+<body>
+     <h2>List Siswa</h2>
+     Pakai Foreach
+     <table border=1>
+          <tr>
+               <th>Nama</th>
+               <th>Kelas</th>
+               <th>Jurusan</th>
+          </tr>
+          <?php foreach($siswa as $key=>$v){
+          ?>
+               <tr>
+                   <td> <?php echo $v['nama']; ?> </td>
+                   <td> <?php echo $v['kelas']; ?> </td>
+                   <td> <?php echo $v['jurusan']; ?> </td>
+               </tr>
+          <?php
+          }?>
+     </table>
+
+     <br/>
+     Pakai While
+     <table border=1>
+          <tr>
+               <th>Nama</th>
+               <th>Kelas</th>
+               <th>Jurusan</th>
+          </tr>
+          <?php 
+               $a = 0;
+               while($a < count($siswa)){
+          ?>
+               <tr>
+                   <td> <?php echo $siswa[$a]['nama']; ?> </td>
+                   <td> <?php echo $siswa[$a]['kelas']; ?> </td>
+                   <td> <?php echo $siswa[$a]['jurusan']; ?> </td>
+               </tr>
+          <?php
+               $a++;
+               }
+          ?>
+     </table>
+</body>
+</html>
